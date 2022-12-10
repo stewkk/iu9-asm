f828b88c17ffac44081c4992202c73b1da5c8ec7
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2022-12-10 14:55:37 +0300

    Add nix integration

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/fd26d2bd8d0e4f3248a3cca0f405fb60117ab988.tar.gz") {} }:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    gnumake
+    yasm
+    gdb
+    gcc
+    nasmfmt
+
+    figlet
+    # keep this line if you use bash
+    bashInteractive
+  ];
+
+  shellHook = ''
+    echo iu9-ASM | figlet
+  '';
+}
